a9bc4c904874e437f6441f81ae80bf1a6baf8864
Ryan Gens <gensy83@gmail.com>
2018-06-02 15:14:52 -0500

    Initial commit

---
+++ ServiceMonitor/Email.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace ServiceMonitor
+{
+    static class Email
+    {
+        public static void SendMail(string processName)
+        {
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("gensttestmail@gmail.com");
+
+            // The important part -- configuring the SMTP client
+            SmtpClient smtp = new SmtpClient();
+            smtp.Port = 587;   // 587 Works for Gmail
+            smtp.EnableSsl = true;
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.UseDefaultCredentials = false; 
+            smtp.Credentials = new NetworkCredential(mail.From.ToString(), "Marshall83");  // [4] Added this. Note, first parameter is NOT string.
+            smtp.Host = "smtp.gmail.com";
+
+            //recipient address
+            mail.To.Add(new MailAddress("gensy83@gmail.com"));
+
+            //Formatted mail body
+            mail.IsBodyHtml = true;
+            string st = $"{processName} has stopped working";
+
+            mail.Body = st;
+            smtp.Send(mail);
+        }
+    }
+}
---
+++ ServiceMonitor/GDSincProcessMonitor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Management;
+using System.Windows.Forms;
+
+namespace ServiceMonitor
+{
+    public partial class GDSincProcessMonitor : ServiceBase
+    {
+        private ManagementEventWatcher processWatch;
+        public GDSincProcessMonitor()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            processWatch = new ManagementEventWatcher(
+                new WqlEventQuery("SELECT * FROM Win32_ProcessStartTrace"));
+            processWatch.EventArrived += new EventArrivedEventHandler(startWatch_EventArrived);
+            processWatch.Start();                        
+        }
+
+        protected override void OnStop()
+        {
+            processWatch.Stop();
+        }
+        public static void ProcessClosed(Object sender, EventArgs e, Process p)
+        {
+            MessageBox.Show($"The {p.ProcessName} process has closed");
+            Email.SendMail(p.ProcessName);
+        }
+
+        static void startWatch_EventArrived(object sender, EventArrivedEventArgs e)
+        {
+            Process[] p = Process.GetProcessesByName("NOTEPAD++");
+            Array.ForEach<Process>(p.ToArray<Process>(), x => {
+
+                Console.WriteLine(x.ProcessName);
+                x.EnableRaisingEvents = true;
+                x.Exited += delegate (Object processSender, EventArgs processEvent) { ProcessClosed(processSender, processEvent, x); };
+            });
+        }
+    }
+}
